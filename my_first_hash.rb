f34c4acdcffef5efe22b6f4a9644c46e9333f9aa
Nguyen Ha <nynha0526@gmail.com>
2019-12-22 06:28:00 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,36 +1,44 @@
 def my_hash
-  my_hash = {hash => "myhash"}
 
-  
+  my_hash = {
+    "my_name" => "Nguyen",
+    "my_height" => "5 feet 0 inches",
+    "my_weight" => "Will remain unknown",
+  }
+
 end
 
 
 def shipping_manifest
-  shipping_manifest = {"whale bone corsets" => "5", "porcelain vases" => "2", "oil paintings" => "3"}
+ 
+  the_manifest = {
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
+                  "oil paintings" => 3
+                 }
 
 end
 
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
 
   shipping_manifest["oil paintings"]
-
 end
 
 def adding
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
+  
   shipping_manifest["muskets"] = 2
+
   shipping_manifest["gun powder"] = 4
-
-
 
   shipping_manifest
 
